6faa3979a7dd2bf4c79ed5313290e583504bf6c0
PastaCodes <marco.j.buda@gmail.com>
2022-09-06 16:16:07 +0200

    Fixed logging

--- src/main/java/org/speac/utilities/LogUtils.java
+++ src/main/java/org/speac/utilities/LogUtils.java
@@ -3,19 +3,20 @@ package org.speac.utilities;
 import org.speac.Start;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public final class LogUtils {
-	private static final String LOG_FILE_PATH = Start.RESOURCES_DIRECTORY + ".log";
+	private static final Path LOG_FILE_PATH = Start.RESOURCES_DIRECTORY.resolve(".log");
 	public static final Logger LOGGER;
 	static {
 		LOGGER = Logger.getLogger("Speac Logger");
 
 		FileHandler handler = null;
 		try {
-			handler = new FileHandler(LogUtils.LOG_FILE_PATH);
+			handler = new FileHandler(LogUtils.LOG_FILE_PATH.toString());
 		} catch (IOException | SecurityException ignored) {}
 		assert handler != null;
 		LOGGER.addHandler(handler);
